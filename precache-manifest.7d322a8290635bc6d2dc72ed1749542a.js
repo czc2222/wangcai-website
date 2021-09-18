e173207d3748ea254f697d4e91a91eac3b98f058
czc222 <1263291450@qq.com>
2021-09-18 17:48:34 +0800

    deploy

---
+++ precache-manifest.7d322a8290635bc6d2dc72ed1749542a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462a0658585a9487a9a6",
+    "url": "/wangcai-website/css/app.5895da48.css"
+  },
+  {
+    "revision": "3eb71ecf5d5ae772b43a0442e75f429f",
+    "url": "/wangcai-website/index.html"
+  },
+  {
+    "revision": "462a0658585a9487a9a6",
+    "url": "/wangcai-website/js/app.ff5091b8.js"
+  },
+  {
+    "revision": "40d1f0982dfa52e5688d",
+    "url": "/wangcai-website/js/chunk-vendors.88bbb2c1.js"
+  },
+  {
+    "revision": "8743e3c6d3a9deb93a5a625838948152",
+    "url": "/wangcai-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/wangcai-website/robots.txt"
+  }
+]);
